5a7f60ab9961d6cbf7436048aea120e3bd545379
carry <zai_wu_chun_qiu@163.com>
2018-07-04 21:39:44 +0800

    first

---
+++ src/main/java/com/github/luutqf/Entity.java
@@ -0,0 +1,22 @@
+package com.github.luutqf;
+
+/**
+ * @Author : ZhenYang
+ * @Despriction :
+ * @Date: Created in 2018/7/4 21:30
+ * @Modify By:
+ */
+public class Entity {
+    private String name = "这是一个测试包";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public static void test(){
+        System.out.println("就是测试一下");
+    }
+}
